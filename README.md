6980481f73fcd2cfd06ac690c0f3dc9a14d73e3d
Thiagodevth <thiagofd341@gmail.com>
2023-12-19 19:51:52 -0300

    atualizando o readme

--- README.md
+++ README.md
@@ -1,25 +1,56 @@
 # Project Node.js
+# Desafio NODE
 
 Projeto na parte de Back-End onde trabalhei os metodos GET, POST PUT e DELETE, e cada um desse tem uma função especifica, (GET)=> Buscar informações no back-end e mostra, (POST)=> Criar informações no back-end,
- (PUT / PATCH)=> Alterar / Atualizar informações no back-end, (DELETE)=> Deletar informaçõs no back-end, esse projeto é voltado para o back-end onde criei essas rotas, e para poder criar, buscar, atualizar e deletar, uso uma API que cria um ID de usuario unico universal, e para este projeto uso um Framework bastante conhecido que o (Express.JS) é um framework para Node.js que fornece recursos mínimos para construção de servidores web, uso tamtem a API (UUID),que gera id unico, Um Identificador Único Universal, que é um número de 128 bits usado para identificar informações em sistemas de computação
+ (PUT / PATCH)=> Alterar / Atualizar informações no back-end, (DELETE)=> Deletar informaçõs no back-end, esse projeto é voltado para o back-end onde criei essas rotas, e para poder criar, buscar, atualizar e deletar, uso uma API que cria um ID de usuario unico universal, e para este projeto uso um Framework bastante conhecido que o (Express.JS) é um framework para Node.js que fornece recursos mínimos para construção de servidores web, uso tamtem a API (UUID),que gera id unico, Um Identificador Único Universal, que é um número de 128 bits usado para identificar informações em sistemas de computação.
+ usei o middleware para mostrar o método e a url de cada requisição, e mais outro middleware para verificar a existência do id passado na requisição
+
+                                 <h1>ROTAS</h1>
+POST /order: A rota deve receber o pedido do cliente, o nome do clientee o valor do pedido,essas informações devem ser passadas dentro do corpo(body) da requisição, e com essas informações você deve registrar o novo pedido dentro de um array no seguinte formato: 
+{ 
+ id: "ac3ebf68-e0ad-4c1d-9822-ff1b849589a8",
+ order: "X- Salada, 2 batatas grandes, 1 coca-cola", 
+ clientName:"José", price: 44.50, 
+ status:"Em preparação" 
+ }.
+  Não se esqueça que o ID deve ser gerado por você, dentro do código utilizando UUID V4, assim que o pedido for criado, você deve sempre colocar o status como "Em preparação".
+
+GET /order: Rota que lista todos os pedidos já feitos.
+
+PUT /order/:id: Essa rota deve alterar um pedido feito já. Pode alterar um ou todos os dados do pedido. idO pedido deve ser enviado nos intervalos da rota.
+
+DELETE /order/:id: Essa rota deve excluir um pedido já feito com o idenviado nos parâmetros da rota.
+
+GET /order/:id: Essa rota recebe os idparâmetros e deve retornar um pedido específico.
+
+PATCH /order/:id: Essa rota recebe o idnas configurações e assim que ela para chamada, deve alterar o status do pedido recebido pelo id para "Pronto".
 
 <br>
 <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" alt="logo-js">
-<img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)  alt="logo-js">
+<img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)"  alt="logo-js">
 <br>
 <br>
-<h1>Rota (POST) Cria</h1>
+<h2>Rota (POST) Cria</h2>
 <img src="./assets/Captura de Tela (51).png">
 <br>
-<h1>Rota (GET) Busca</h1>
+<h2>Rota (GET) Busca</h2>
 <img src="./assets/Captura de Tela (52).png">
 <br>
-<h1>Rota (DELETE) Deleta</h1>
+<h2>Rota (DELETE) Deleta </h2>
 <img src="./assets/Captura de Tela (54).png">
 <br>
-<h1>Rota (PUT) GET mostra usuario atualizado por PUT</h1>
+<h2>Rota (PUT) GET mostra usuario atualizado por PUT</h2>
 <img src="./assets/Captura de Tela (55).png">
 <br>
-<h1>Rota (DELETE) GET mostra usuario deletado por DELETE</h1>
+<h2>Rota (DELETE) GET mostra usuario deletado por DELETE</h2>
 <img src="./assets/Captura de Tela (56).png">
+<br>
+<h2>Rota (POST) <h3>usuario está com o pedido e o status está em preparação</h3></h2>
+<img src="./assets/Captura de Tela (61).png">
+<br>
+<h2>Rota (PATCH) <h3>PATCH atualiza o status do pedido para pronto</h3></h2>
+<img src="./assets/Captura de Tela (62).png">
+<br>
+<h2>Rota (DELETE) GET mostra usuario deletado por DELETE</h2>
+<img src="./assets/Captura de Tela (63).png">
 <br>
--- index.js
+++ index.js
@@ -17,69 +17,109 @@
 
 */
 // importar framework (express) para o index.js
-const express = require('express')// Framework
+const express = require('express')
 const uuid = require('uuid')// bibioteca (id) unico
 const port = 3000
 const app = express()
 app.use(express.json()) // estou avisando que o (express) vai usar por padrão (json)
 
-const users = []
-// 5º => O Middleware recebe 3 parametros, request, response, next
+const users = [] //array de pedidos
+
+/* => O Middleware recebe 3 parametros, request, response, next
+  middleware para verificar a existência do id passado na requisição
+*/
 const checkUserId = (request, response, next) => {
     const { id } = request.params // id do usuario
+
     const index = users.findIndex(user => user.id === id)
-    if(index < 0){
-        return response.status(404).json({error: 'user not found'})
+    if (index < 0) {
+        return response.status(404).json({ error: 'Pedido não encontrado' })
     }
 
     request.userIndex = index
     request.userId = id
     next() // o next continua o fluxo da aplicação
-
 }
-//  1º Criando rota com metodo =>get, endereço da rota('/users')
-app.get('/users', (request, response) => {
-  
-    // pardão json / forma do front-end se conectar com o back-end
-    return response.json(users) // quando a chave e o valor são iguais posso fazer assim, o js entende 
-//   return response.json({ name: name, age: age })
-})
-//  2º Criando rota com metodo =>post
-app.post('/users', (request, response) => {
-    const { name, age } = request.body
 
-    const user = { id: uuid.v4(), name, age }
+//middleware para mostrar o método e a url de cada requisição
+const requestType = (request, response, next) =>{
+    console.log(`method: ${request.method}, URL: http://localhost:3000${request.url}`)
+    
+    next()
+}
+
+/*
+POST /order: A rota deve receber o pedido do cliente, o nome do cliente e o valor do pedido, essas informações devem 
+ser passadas dentro do corpo (body) da requisição, e com essas informações você deve registrar o novo 
+pedido dentro de um array no seguinte formato:
+
+ { id: "ac3ebf68-e0ad-4c1d-9822-ff1b849589a8",
+  order: "X- Salada, 2 batatas grandes, 1 coca-cola", 
+  clientName:"José", 
+  price: 44.50, 
+  status:"Em preparação" 
+}.
+
+Não se esqueça que o ID deve ser gerado por você, dentro do código utilizando UUID V4,
+ assim que o pedido for criado, você deve sempre colocar o status como "Em preparação".
+*/
+
+//  1º Criando rota com metodo =>post
+app.post('/order', requestType, (request, response) => {
+    const { order, clienteName, price, status } = request.body
+
+    const user = { id: uuid.v4(), order, clienteName, price, status }
 
     users.push(user)
 
-    return response.status(201).json(users)
+    return response.status(201).json(user)
 })
+
+//  2º Criando rota com metodo =>get, endereço da rota('/users')
+app.get('/order', requestType, (request, response) => {
+
+    // pardão json / forma do front-end se conectar com o back-end
+    return response.json(users) // quando a chave e o valor são iguais posso fazer assim, o js entende 
+    //   return response.json({ name: name, age: age })
+})
+
 //  3º Criando rota com metodo =>put
-app.put('/users/:id', checkUserId, (request, response) => {
-    const { name, age } = request.body // informações do usuario pelo corpo
+app.put('/order/:id', checkUserId, requestType, (request, response) => {
+    
+    const { order, clienteName, price, status } = request.body
     const index = request.userIndex
     const id = request.userId
-    
-    const updateUser = { id, name, age }// atualiza usuario
+
+    const updateUser = { id, order, clienteName, price, status }// atualiza usuario
 
     users[index] = updateUser
 
     return response.json(updateUser)
 })
-//  4º Criando rota com metodo =>delete
-app.delete('/users/:id', checkUserId, (request, response) => {
-    const index = request.userIndex
-    users.splice(index,1)
-    return response.status(204).json()
+
+app.patch('/order/:id', checkUserId, requestType, (request, response) => {
+    
+    const { id } = request.params
+    const { status } = request.body
+
+    //const up = {id, order, clienteName, price, status }
+    const index = users.findIndex(order => order.id === id)
+    if (index === -1) {
+        return response.status(404).json({error: "Pedido não encontrado"})
+    }
+    
+    users[index].status = status || "Pronto"
+
+    return response.json(users)
 })
 
+//  4º Criando rota com metodo =>delete
+app.delete('/order/:id', requestType, (request, response) => {
+    const index = request.userIndex
+    users.splice(index, 1)
 
-
-
-
-
-
-
+    return response.status(204).json()
+})
 
 //app.listen(3000) // porta 3000 posso fazer assim ou👇
 app.listen(port, () => {
